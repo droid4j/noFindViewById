5f355d498a6224d6d2b7427b30a93726b49a7a0f
droid4j <android4j@163.com>
2020-02-22 17:26:37 +0800

    [update] 使用Reflect重写注入方法

--- src/main/java/com/dapan/nofindviewbyid/ViewUtils.java
+++ src/main/java/com/dapan/nofindviewbyid/ViewUtils.java
@@ -3,27 +3,24 @@ package com.dapan.nofindviewbyid;
 import android.app.Activity;
 import android.view.View;
 
+import com.dapan.reflectlib.Reflect;
+
 import java.lang.reflect.Field;
 
 public class ViewUtils {
     public static void inject(final Activity activity) {
 
-        Field[] fields = activity.getClass().getDeclaredFields();
-        for (Field field : fields) {
-            View result = null;
-            ViewById viewById = field.getAnnotation(ViewById.class);
-            if (viewById != null) {
-                int id = viewById.value();
-                result = activity.findViewById(id);
-            }
-            if (result != null) {
-                try {
-                    if (!field.isAccessible()) field.setAccessible(true);
-                    field.set(activity, result);
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
+        Reflect.inject(activity.getClass()).set(activity, new Reflect.Callback<View>() {
+            @Override
+            public View getValue(Field field) {
+                View result = null;
+                ViewById viewById = field.getAnnotation(ViewById.class);
+                if (viewById != null) {
+                    int id = viewById.value();
+                    result = activity.findViewById(id);
                 }
+                return result;
             }
-        }
+        });
     }
 }
